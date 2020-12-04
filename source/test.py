8df404a75a0ff553a77e9b65b4366bc8bf23ee48
GiaPhuc <phucgia1808@gmail.com>
2020-12-04 16:02:37 +0800

    prove P1 => f(x3,x4) and P2 /\ f(x3,x4) => property. Only P2 /\ f(x3,x4) => property is valid and it may be because P2 => property is valid. Test on sample come from the layer before and after relu layer

--- source/test.py
+++ source/test.py
@@ -3,6 +3,7 @@ import json
 from solver.deepcegar_impl import Poly
 from json_parser import parse
 from sklearn import svm
+from z3 import *
 from model.lib_models import Model
 from utils import *
 from pathlib import Path
@@ -44,8 +45,8 @@ def generate_sample(model, layer_idx, lst_poly):
     else:       
         no_neuron = len(lst_poly[layer_idx].lw)
         shape = np.array([1, no_neuron])
-        lower = lst_poly[layer_idx].lw
-        upper = lst_poly[layer_idx].up
+        lower = lst_poly[layer_idx + 1].lw
+        upper = lst_poly[layer_idx + 1].up
         new_model = Model(shape, lower, upper, model.layers[layer_idx+1:], None)
 
     size = np.prod(new_model.shape)
@@ -53,6 +54,7 @@ def generate_sample(model, layer_idx, lst_poly):
     n = 10000
     generate_y = np.zeros((n, size))
     input_x = np.zeros((n, size))
+
     
     for i in range(n):
         x0 = generate_x(size, new_model.lower, model.upper)
@@ -61,6 +63,53 @@ def generate_sample(model, layer_idx, lst_poly):
         input_x[i] = x0
     
     return input_x, generate_y
+
+def prove(coef, intercept):
+    s = Solver()
+    x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12 = Reals('x1 x2 x3 x4 x5 x6 x7 x8 x9 x10 x11 x12')
+    
+    P1 = And([x1 >= -1, x1 <= 1, x2 >= -1, x2 <= 1, x3 <= x1 + x2, x3 >= x1 + x2, x4 <= x1 - x2, x4 >= x1 - x2])
+    P2 = And([x5 >= 0, x5 <= 0.5*x3 + 1, x6 >= 0, x6 <= 0.5*x4 + 1,
+              x7 >= x5 + x6, x7 <= x5 + x6, x8 >= x5 - x6, x8 <= x5 - x6, x9 >= x7, x9 >= x7, x10 >= 0, x10 <= 0.5*x8 + 1,
+              x11 >= x9 + x10 + 1, x11 <= x9 + x10 + 1, x12 >= x10, x12 <= x10])
+    f = coef[0]*x3 + coef[1]*x4 + intercept == 0
+    
+    Property = Or(ForAll([x3, x4], x12 > x11), ForAll([x3, x4], x12 < x11))
+              
+    s.push()
+    s.add(Not(Implies(And(P1, P2), Property))) #if unsat then Property hold
+    r = s.check()
+    print(r)
+    if r == unsat:
+        print("Property hold")
+    s.pop()
+    
+    s.push()
+    s.add(Not(Implies(P1, f))) #if unsat then P1 => f is valid
+    r = s.check()
+    print(r)
+    if r == unsat:
+        print("P1 => f is valid")
+    else:
+        print("P1 => f is not valid")       
+    s.pop()
+    
+    s.add(Not(Implies(And(P2, f), Property))) #if unsat then P2 and f => Property is valid
+
+    r = s.check()
+    print(r)
+    if r == unsat:
+        print("P2 and f => Property is valid")
+    else:
+        print("P2 and f => Property is not valid")
+
+    s.add(Not(Implies(P2, Property))) #if unsat then P2 => Property is valid
+    r = s.check()
+    print(r)
+    if r == unsat:
+        print("P2 => Property is valid")
+    else:
+        print("P2 => Property is not valid")
         
 
 def main():
@@ -74,16 +123,34 @@ def main():
     
     model, assertion, solver, display = parse(spec)
 
+    #Sample before relu layer
+    print("Sample before relu layer")
     lst_poly = buildDeepPoly(model)
     input_x, generate_y = generate_sample(model, 0, lst_poly)
 
     label = [a == b for a, b in zip(np.argmax(input_x, axis=1), np.argmax(generate_y, axis=1))]
+            
     label = np.array(label, dtype=int)
+
     clf = svm.SVC(kernel="linear")
     clf.fit(generate_y, label)
     print(clf.coef_)
     print(clf.intercept_)
+    prove(clf.coef_[0], clf.intercept_[0])
 
+    print()
+    print("Sample after relu layer")
+    #Sample after relu layer 
+    lst_poly = buildDeepPoly(model)
+    input_x, generate_y = generate_sample(model, 1, lst_poly)
+
+    label = [a == b for a, b in zip(np.argmax(input_x, axis=1), np.argmax(generate_y, axis=1))]
+    label = np.array(label, dtype=int)
+    clf = svm.LinearSVC()
+    clf.fit(generate_y, label)
+    print(clf.coef_)
+    print(clf.intercept_)
+    prove(clf.coef_[0], clf.intercept_[0])
 
     
 
